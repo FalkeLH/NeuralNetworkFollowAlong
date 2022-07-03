af76c29ccb66d29114105eacaeac7999e198918a
Falke Lindegaard Hvidtfeldt <falkelh@icloud.com>
2022-07-03 14:24:22 +0200

    Removed unnecessary part of iterator, program should still have the same functionality

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
 fn main() {
-    let inputs = vec![1.2, 5.1, 2.1];
-    let weights = vec![3.1, 2.1, 8.7];
-    let bias = 3.;
+    let inputs = vec![1., 2., 3., 4.];
+    let weights = vec![0.2, 0.8, -0.5, 0.9];
+    let bias = 2.;
     
     let mut output = 0.;
     inputs
         .into_iter()
         .zip(weights)
         .map(|(i, w)| i * w)
-        .collect::<Vec<f64>>()
-        .into_iter()
         .for_each(|e| output += e);
     let output = output + bias;
 
