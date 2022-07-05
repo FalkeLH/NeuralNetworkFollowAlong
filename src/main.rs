93b225e269cceef7dab541da446d9e21f422414c
Falke Lindegaard Hvidtfeldt <falkelh@icloud.com>
2022-07-05 14:04:17 +0200

    Make input a matrix and implement dot product for it

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 fn main() {
     // Fetch basic inputs, weights and biases
-    let inputs = vec![1., 2., 3., 2.5];
+    let inputs = vec![vec![1., 2., 3., 2.5],
+                      vec![2., 5., -1., 2.],
+                      vec![-1.5, 2.7, 3.3, -0.8]];
     let weights = vec![
         vec![0.2, 0.8, -0.5, 1.],
         vec![0.5, -0.91, 0.26, -0.5],
@@ -10,12 +12,14 @@ fn main() {
 
     // Make sure there are an equal amount of biases and weights. Calculate the final output as well
     assert_eq!(weights.len(), bias.len());
-    let mut output: Vec<f64> = vec![0.; weights.len()];
+    let mut output: Vec<Vec<f64>> = vec![vec![0.; weights.len()]; inputs.len()];
     for i in 0..weights.len() {
-        for j in 0..weights[i].len() {
-            output[i] += inputs[j] * weights[i][j];
+        for j in 0..inputs.len() {
+            for k in 0..weights[i].len() {
+                output[j][i] += inputs[j][k] * weights[i][k];
+            }
+        output[j][i] += bias[i]
         }
-        output[i] += bias[i]
     }
 
     println!("{:?}", output);
